8c3576fffd60f36c21c8c989f5e2cec746e56eb9
Dan Moore <bitbucket@mooreds.com>
2016-07-14 17:57:00 +0000

    added support for rails caching

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -1,17 +1,11 @@
-class SitemapController < ActionController::Metal
-  include AbstractController::Rendering
-  include ActionController::MimeResponds
-  include ActionController::DataStreaming
-  include ActionController::RackDelegation
-  include ActionController::Rescue
-  include ActionController::Head
+class SitemapController < ApplicationController
 
   def sitemap
-    if false
-      redirect_to "/" # The CDN then fetches the sitemap.xml from the sitemap/generate URL which points to the `generate` action
-    else
-      render_site_map
-    end
+    #if false
+      #redirect_to "/" # The CDN then fetches the sitemap.xml from the sitemap/generate URL which points to the `generate` action
+    #else
+    render_site_map
+    #end
   end
 
   def generate
@@ -23,10 +17,17 @@ class SitemapController < ActionController::Metal
     def render_site_map
       respond_to do |format|
 
+        data = Rails.cache.fetch(@current_community, expires_in: 24.hours) do
           adapter = SitemapGenerator::NeverWriteAdapter.new
 
+          if @current_community.use_domain 
+            default_host = "https://"+@current_community.domain
+          else 
+            default_host = "https://"+APP_CONFIG.domain
+          end
+
           SitemapGenerator::Sitemap.create(
-                :default_host => 'http://example2.com',
+                :default_host => default_host,
                 :adapter => adapter) do
             Listing.where(deleted: false, open: true, community_id: 1).find_each do |listing|
               add listing_path(listing), :lastmod => listing.updated_at
@@ -35,8 +36,9 @@ class SitemapController < ActionController::Metal
 
           #SitemapGenerator::Interpreter.run(:default_host => "http://www.bar.com", :adapter => adapter)
           data = adapter.get_data
-          format.xml_gz { send_data  ActiveSupport::Gzip.compress(data) }
-          format.html { send_data  ActiveSupport::Gzip.compress(data) }
+        end
+        format.xml_gz { send_data  ActiveSupport::Gzip.compress(data) }
+        format.html { send_data  ActiveSupport::Gzip.compress(data) }
       end
     end
 end
