3c1dd0ff3561a814c8c2477ebb9ae3dab849c12a
Dan Moore <bitbucket@mooreds.com>
2016-07-14 19:57:29 +0000

    not needed, all done at runtime

--- config/sitemap.rb
+++
@@ -1,7 +0,0 @@
-# Set the host name for URL creation
-
-#SitemapGenerator::Sitemap.create do
-  #Listing.where(deleted: false, open: true).find_each do |listing|
-    #add listing_path(listing), :lastmod => listing.updated_at
-  #end
-#end
